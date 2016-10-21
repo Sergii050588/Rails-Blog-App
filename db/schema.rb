a055cfce0728572b604efce7bee3d91de249ecef
Sergii Melnichenko <mozgoved88@mail.ru>
2016-10-21 13:14:03 +0300

    User model added with article-user_id connection

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203020035) do
+ActiveRecord::Schema.define(version: 20161021094858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141203020035) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -32,5 +33,12 @@ ActiveRecord::Schema.define(version: 20141203020035) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
